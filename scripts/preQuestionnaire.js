dd9b855d3818d726864e3b8358bd46bbb3f16803
Dennis Rodrigo da Cunha Silva <denisrcs@gmail.com>
2018-03-29 23:47:19 +0000

    adding gender and age to pre questionnaire

--- scripts/preQuestionnaire.js
+++ scripts/preQuestionnaire.js
@@ -3,11 +3,16 @@ $( document ).ready(function(){
 	var timestamp = now.toISOString();
 	localStorage.setItem('startTime',timestamp);
 	
-	var session = localStorage.getItem("Session");
+	var gender = "Female";
+	$('#gender').change(function () {
+		gender = $(this).find("option:selected").text().replace(/\s/g,'');
+	});
 	
+	var session = localStorage.getItem("Session");
 	if (session != "session1"){
 	    $("#entry-pc-usage-week").prop("required", false);
-	    $("#entry-pc-usage-life").prop("required", false);    
+	    $("#entry-pc-usage-life").prop("required", false);
+	    $("#entry-age").prop("required", false);    
 	    $(".survey_computer_usage").css("display", "none");
 	}
 	
@@ -16,13 +21,19 @@ $( document ).ready(function(){
         var valenceChecked = $("input[name=valenceradio]:checked").val();
         var weeklyUsage = $("#entry-pc-usage-week").val();
         var lifeUsage = $("#entry-pc-usage-life").val();
+        var age = $("#entry-age").val();
+        
         if (weeklyUsage == "") weeklyUsage = "-1";
-        if (lifeUsage == "") lifeUsage = "-1";
+        if (lifeUsage == "")   lifeUsage = "-1";
+        if (age == "")          age = "-1";
+        
         var response = {
-            "arousal" : arousalChecked,
-            "valence" : valenceChecked,
+            "arousal"     : arousalChecked,
+            "valence"     : valenceChecked,
             "weeklyUsage" : weeklyUsage,
-            "lifeUsage" : lifeUsage
+            "lifeUsage"   : lifeUsage,
+            "age"         : age,
+            "gender"      : gender, 
         };
         
         localStorage.setItem("pre-questionnaire", JSON.stringify(response));
